aa158891376347bbc90273523a347ca5eae4b99d
Charles Lowell <cowboyd@frontside.io>
2016-08-25 22:28:21 -0500

    make a simple reboot, with a simple state class
    In order to make forward progress, this brings back the absolute
    simplest thing that could possibly work: a bare-bones node/mocha test
    harness that can be iterated on quickly.
    
    For reasons of debuggability and cross browser testability, we will need
    to sort out how to get these tests running in the browser at some point,
    but we can do that in parallel with the actual work on the microstate
    library.
    
    Included with this change is the basic `State` object that wraps any
    JavaScript object value. It contains the four basic operations common to
    all microstates `set`/`assign`/`put`/`delete`. In the end, I decided not
    to make a separate branch of microstate for primitive types such as
    `number`, `boolean` etc... since they _can_ wrap primitive values.

--- src/primitives/string.js
+++
@@ -1,7 +0,0 @@
-import Primitive from './primitive';
-
-export default Primitive.extend('StringState', {
-  constructor(value = '') {
-    return { value: String(value) };
-  }
-});
---
+++ test/state-test.js
@@ -0,0 +1,124 @@
+import { describe, beforeEach, it } from 'mocha';
+import { expect } from 'chai';
+
+import State from '../src/state';
+
+describe("The Basic Opaque State", function() {
+  let state, value, next;
+
+  describe("wrapping a primitive value in a State", function() {
+    beforeEach(function() {
+      state = new State(5);
+    });
+
+    it("has that value as its primitive value", function() {
+      expect(state.valueOf()).to.eq(5);
+    });
+    it("has no enumerable keys", function() {
+      expect(Object.keys(state)).to.deep.equal([]);
+    });
+  });
+
+  describe("wrapping an object value in a State", function() {
+    let object;
+    beforeEach(function() {
+      object = { ohai: 'There', hello: 'World' };
+      state = new State(object);
+    });
+
+    it("has that object as its primitive value", function() {
+      expect(state.valueOf()).to.eq(object);
+    });
+
+    it("has the enumerable keys of the object value", function() {
+      expect(Object.keys(state)).to.deep.equal(['ohai', 'hello']);
+    });
+
+    it("has the same property values as the object value", function() {
+      expect(state.ohai).to.equal('There');
+      expect(state.hello).to.equal('World');
+    });
+
+    describe("setting the value to a new object", function() {
+      beforeEach(function() {
+        next = state.set({totally: 'different'});
+      });
+
+      it("creates a completely new state", function() {
+        expect(next).not.to.eq(state);
+      });
+      it("no longer has any of the old properties", function() {
+        expect(next.ohai).to.equal(undefined);
+        expect(next.hello).to.equal(undefined);
+      });
+      it("has the properties of the new object", function() {
+        expect(next.totally).to.equal('different');
+      });
+      it("has the enumerable keys of the new object", function() {
+        expect(Object.keys(next)).to.deep.equal(['totally']);
+      });
+    });
+
+    describe("assigning new properties to the state", function() {
+      beforeEach(function() {
+        next = state.assign({ how: 'do you do?'});
+      });
+      it("maintains the list of old properties", function() {
+        expect(next.ohai).to.equal('There');
+        expect(next.hello).to.equal('World');
+      });
+      it("adds the new property", function() {
+        expect(next.how).to.equal('do you do?');
+      });
+      it("has the new enumerable key", function() {
+        expect(Object.keys(next)).to.deep.equal(['ohai', 'hello', 'how']);
+      });
+    });
+
+    describe("putting a single property into the state", function() {
+      beforeEach(function() {
+        next = state.put('how', 'do you do?');
+      });
+      it("maintains the list of old properties", function() {
+        expect(next.ohai).to.equal('There');
+        expect(next.hello).to.equal('World');
+      });
+      it("adds the new property", function() {
+        expect(next.how).to.equal('do you do?');
+      });
+      it("has the new enumerable key", function() {
+        expect(Object.keys(next)).to.deep.equal(['ohai', 'hello', 'how']);
+      });
+    });
+
+    describe("deleting a property", function() {
+      beforeEach(function() {
+        next = state.delete('ohai');
+      });
+      it("removes that property", function() {
+        expect(next.ohai).to.equal(undefined);
+      });
+      it("maintains the other properties", function() {
+        expect(next.hello).to.equal('World');
+      });
+      it("only has the remaining property name in its enumerable ", function() {
+        expect(Object.keys(next)).to.deep.equal(['hello']);
+      });
+    });
+
+    describe("trying to manipulate properties directly on a state", function() {
+      let edit, add, remove;
+      beforeEach(function() {
+        try { state.ohai = 'blork'; } catch(e) { edit = e; }
+        try { state.say = 'what?'; } catch(e) { add = e; }
+        try { delete state.hello; } catch(e) { remove = e; }
+      });
+      it("fails because the state is immutable 🙌", function() {
+        expect(edit).to.be.instanceof(TypeError);
+        expect(add).to.be.instanceof(TypeError);
+        expect(remove).to.be.instanceof(TypeError);
+      });
+    });
+
+  });
+});
