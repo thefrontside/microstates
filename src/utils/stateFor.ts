526dd221a1dc1a1bff2ac163d9325b9cf9589d49
Taras Mankovski <tarasm@gmail.com>
2017-09-26 00:46:12 +0400

    Removed unncessary stateFor function

--- src/utils/stateFor.ts
+++
@@ -1,8 +0,0 @@
-import { ITypeTree, IState } from '../Interfaces';
-import * as view from 'ramda/src/view';
-import * as __ from 'ramda/src/__';
-import mapState from './mapState';
-
-export default function stateFor(tree: ITypeTree, value: any): IState {
-  return mapState(tree, [], view(__, value));
-}
