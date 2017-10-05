48996c67d1a4db161ca5b8923f5af0e3f8e74396
Charles Lowell <cowboyd@frontside.io>
2017-10-05 09:11:05 -0500

    dev checkpoint.

--- src/utils/transitions-for.js
+++ src/utils/transitions-for.js
@@ -1,40 +1,14 @@
-import { reduceObject } from 'ioo';
-import mergeDeepRight from 'ramda/src/mergeDeepRight';
-
-import MicrostateObject from '../primitives/object';
 import getReducerType from './get-reducer-type';
-import getTypeDescriptors from './get-type-descriptors';
 import transition from './transition';
+
+import { append, map, filter } from 'funcadelic';
+
+const set = transition(function set(current, state) {
+  return state && state.valueOf ? state.valueOf() : state;
+});
 
 export default function transitionsFor(Type) {
   let type = getReducerType(Type);
-  let transitions = reduceObject(
-    getTypeDescriptors(type.prototype),
-    (accumulator, descriptor, name) => {
-      return Object.assign({}, accumulator, {
-        [name]: name === 'initialize' ? descriptor.value : transition(descriptor.value),
-      });
-    },
-    {
-      set: transition(function set(current, state) {
-        return state && state.valueOf ? state.valueOf() : state;
-      }),
-    }
-  );
-  let isComposed = type === Type;
-  if (isComposed || type === MicrostateObject) {
-    transitions = Object.assign({}, transitions, {
-      merge: transition(function merge(current, state) {
-        return mergeDeepRight(current, state && state.valueOf ? state.valueOf() : state);
-      }),
-    });
-  }
-  if (isComposed && !transitions.initialize) {
-    transitions = Object.assign({}, transitions, {
-      initialize: function initialize(current, ...args) {
-        return new Type(...args);
-      },
-    });
-  }
-  return transitions;
+  let transitionFns = filter(({ key }) => !['constructor', 'initialize'].includes(key), type.prototype);
+  return append({ set }, map(fn => transition(fn), transitionFns));
 }
--- src/utils/tree.js
+++ src/utils/tree.js
@@ -1,35 +1,28 @@
-import { Foldable, foldl, foldr, Functor, map } from 'funcadelic';
-import { reduceObject } from 'ioo';
+import { filter, append, Functor, map } from 'funcadelic';
 
-import defineComputedProperty from './define-computed-property';
 import getReducerType from './get-reducer-type';
-import isList from './is-list';
-import isPrimitive from './is-primitive';
-import propertiesFor from './properties-for';
-import transitionsFor from './transitions-for';
 
-class TypeTree {
-  constructor() {
-    this.data = null;
+export default class Tree {
+
+  get collapsed() {
+    return append(this.data, map(child => child.collapsed, this.children));
   }
-  static from(Type, path = []) {
-    let primitive = isPrimitive(Type);
-    let [name] = path.slice(-1);
-    let list = isList(Type);
-    return Object.create(TypeTree.prototype, {
+
+  static create({data = ()=> ({}), children =  ()=> ({})}) {
+    return Object.create(Tree.prototype, {
       data: {
-        get() {
-          return {
-            name,
-            path,
-            type: getReducerType(Type),
-            schemaType: Type,
-            isPrimitive: primitive,
-            isComposed: !primitive,
-            isList: list,
-            transitions: transitionsFor(Type),
-          };
-        },
+        get: data
+      },
+      children: {
+        get: children
+      }
+    });
+  }
+
+  static from(Type, path = []) {
+    return Tree.create({
+      data() {
+        return { path, Type };
       },
       /**
        * children property is a map of composed types. When read, it will
@@ -53,56 +46,34 @@ class TypeTree {
        *
        * Eager evaluation would cause infinite loop because parent would be evaluated recursively.
        */
-      children: {
-        get() {
-          return reduceObject(
-            propertiesFor(getReducerType(Type)),
-            (accumulator, type, propName) => {
-              return Object.assign({}, accumulator, {
-                [propName]: TypeTree.from(type, [...path, propName]),
-              });
-            }
-          );
-        },
-      },
+      children() {
+        let Reducer = getReducerType(Type);
+        let childTypes = filter(({ value }) => !!value.call, new Reducer());
+
+        return map((ChildType, key) => Tree.from(ChildType, append(path, key)), childTypes);
+      }
     });
   }
 }
 
-Foldable.instance(TypeTree, {
-  foldl(fn, initial = {}, tree) {
-    return foldl((memo, args) => fn(memo, args), initial, tree.children);
-  },
-  foldr(fn, initial = {}, tree) {
-    return foldr((memo, args) => fn(memo, args), initial, tree.children);
-  },
-});
-
-Functor.instance(TypeTree, {
+Functor.instance(Tree, {
   /**
    * Lazily invoke callback on every proprerty of given tree,
    * the return value is assigned to property value.
-   * 
+   *
    * @param {*} fn (TypeTree, path) => any
    * @param {*} tree Tree
    */
   map(fn, tree) {
-    return foldl(
-      (memo, { key, value }) =>
-        defineComputedProperty(
-          memo,
-          key,
-          function computePropertyValue() {
-            return map(fn, value);
-          },
-          {
-            enumerable: true,
-          }
-        ),
-      fn(tree.data, tree.data.path),
-      tree
-    );
-  },
+    return Object.create(Tree, {
+      data: {
+        get() { return fn(tree.data); }
+      },
+      children: {
+        get() {
+          return map(child => map(fn, child), tree.children);
+        }
+      }
+    });
+  }
 });
-
-export default TypeTree;
