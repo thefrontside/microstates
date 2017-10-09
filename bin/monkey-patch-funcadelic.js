0a930f25552f99f7ef42d63420b960ad6409ce8e
Charles Lowell <cowboyd@frontside.io>
2017-10-09 16:58:51 -0500

    format unmonkey-patch

--- bin/monkey-patch-funcadelic.js
+++ bin/monkey-patch-funcadelic.js
@@ -3,6 +3,6 @@ const json = require('jsonfile');
 const funcadelicPackageFilename = 'node_modules/funcadelic/package.json';
 
 let pkg = json.readFileSync(funcadelicPackageFilename);
-json.writeFileSync(`${funcadelicPackageFilename}.original`, pkg);
+json.writeFileSync(`${funcadelicPackageFilename}.original`, pkg, { spaces: 2 });
 pkg.main = pkg.module;
 json.writeFileSync(funcadelicPackageFilename, pkg, {spaces: 2});
