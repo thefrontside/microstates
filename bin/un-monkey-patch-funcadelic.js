f302b7e89fbaa9036531378426244ac3bcd4ea35
Charles Lowell <cowboyd@frontside.io>
2017-10-09 16:35:28 -0500

    Only monkeypatch funcadelic on test runs.
    Only Jest has a problem with funcadelic, so let's just monkey patch it
    there.

--- bin/un-monkey-patch-funcadelic.js
+++ bin/un-monkey-patch-funcadelic.js
@@ -1,8 +1,7 @@
 const json = require('jsonfile');
 
 const funcadelicPackageFilename = 'node_modules/funcadelic/package.json';
+const originalFuncadelicPackageFilename = 'node_modules/funcadelic/package.json.original';
 
-let pkg = json.readFileSync(funcadelicPackageFilename);
-json.writeFileSync(`${funcadelicPackageFilename}.original`, pkg);
-pkg.main = pkg.module;
+let pkg = json.readFileSync(originalFuncadelicPackageFilename);
 json.writeFileSync(funcadelicPackageFilename, pkg, {spaces: 2});
