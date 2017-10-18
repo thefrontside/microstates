33c56dd5b10589f3e9e59f658637d3b39c9fbd23
Taras Mankovski <tarasm@gmail.com>
2017-10-18 17:24:49 +0800

    Removed unused Computed Property code

--- src/utils/define-computed-property.js
+++
@@ -1,5 +0,0 @@
-import ComputedProperty from './computed-property';
-
-export default function defineComputedProperty(object, name, callback, attributes) {
-  return Object.defineProperty(object, name, new ComputedProperty(callback, attributes));
-}
