dc37352127e29275d234676934ba940c60048443
Taras Mankovski <tarasm@gmail.com>
2017-10-24 13:59:23 +0530

    Added test for using Microstates.to (#31)

--- tests/microstates.test.js
+++ tests/microstates.test.js
@@ -365,4 +365,32 @@ describe('microstates', () => {
       });
     });
   });
+  describe('cart example', () => {
+    class Cart {
+      products = Array;
+      get price() {
+        return this.products.reduce((acc, product) => acc + product.quantity * product.price, 0);
+      }
+      get count() {
+        return this.products.reduce((acc, product) => acc + product.quantity, 0);
+      }
+    }
+    describe('adding products without initial value', () => {
+      let ms;
+      beforeEach(() => {
+        ms = Microstates.from(Cart);
+        ms = ms.to(ms.transitions.products.push({ quantity: 1, price: 10 }));
+        ms = ms.to(ms.transitions.products.push({ quantity: 2, price: 20 }));
+      });
+      it('adds items to the cart', () => {
+        expect(ms).toMatchObject({
+          states: {
+            price: 50,
+            count: 3,
+            products: [{ quantity: 1, price: 10 }, { quantity: 2, price: 20 }],
+          },
+        });
+      });
+    });
+  });
 });
