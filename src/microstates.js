0d18613243b21f5194bc04868b34defb30f4be06
Taras Mankovski <tarasm@gmail.com>
2017-10-28 14:58:37 +0530

    Moved states and transitions to their own files

--- src/microstates.js
+++ src/microstates.js
@@ -1,39 +1,12 @@
-import { append, map } from 'funcadelic';
-import lensPath from 'ramda/src/lensPath';
-
-import gettersFor from './utils/getters-for';
-import initialize from './utils/initialize';
-import isPrimitive from './utils/is-primitive';
-import transitionsFor from './utils/transitions-for';
 import Tree from './utils/tree';
+import States from './utils/states';
+import Transitions from './utils/transitions';
 import validate from './utils/validate';
 
 export default class Microstates {
   constructor(tree, value) {
-    let values = map(data => append(data, { value: initialize(data, value) }), tree);
-
-    this.states = map(
-      ({ Type, value }) => (isPrimitive(Type) ? value : append(value, gettersFor(Type))),
-      values
-    ).collapsed;
-
-    // raw, uncurried transitions
-    let rawTransitions = map(
-      ({ Type, path }) => ({ path, transitions: transitionsFor(Type) }),
-      tree
-    );
-
-    // curry in the lens and value
-    let curriedTransitions = map(
-      ({ path, transitions }) =>
-        map(
-          t => (...args) => t(lensPath(path), map(({ value }) => value, values).collapsed, ...args),
-          transitions
-        ),
-      rawTransitions
-    );
-
-    this.transitions = curriedTransitions.collapsed;
+    this.states = States(tree, value);
+    this.transitions = Transitions(tree, this.states);
     this.tree = tree;
   }
   /**
