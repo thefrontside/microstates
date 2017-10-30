29620e57d0033b8b03e5bde020955ea208330f3e
Taras Mankovski <tarasm@gmail.com>
2017-10-30 20:05:28 +0530

    Added Array.replace (#36)

--- src/types/array.js
+++ src/types/array.js
@@ -1,3 +1,7 @@
+import set from 'ramda/src/set';
+import indexOf from 'ramda/src/indexOf';
+import lensPath from 'ramda/src/lensPath';
+
 export default class ArrayType {
   constructor(value = []) {
     return value instanceof Array ? value : [value];
@@ -10,5 +14,26 @@ export default class ArrayType {
   }
   map(current, callback) {
     return Array.prototype.map.call(current, callback);
+  }
+  /**
+   * Return a new array with first occurance of found item 
+   * replaced with the replacement. It is very optimistic and
+   * will not throw even when item is not found. 
+   * 
+   * ```js
+   * let ms = Microstates.from(MS.Array, ['a', 'b', 'c']);
+   * // => [ d, b, c ]
+   * ```
+   * @param {Array} current 
+   * @param {any} item 
+   * @param {any} replacement 
+   */
+  replace(current, item, replacement) {
+    let index = indexOf(item, current);
+    if (index === -1) {
+      return current;
+    } else {
+      return set(lensPath([index]), replacement, current);
+    }
   }
 }
---
+++ tests/types/array.test.js
@@ -0,0 +1,22 @@
+import 'jest';
+
+import ArrayType from '../../src/types/array';
+import Microstates, * as MS from '../../src';
+
+describe('array', () => {
+  describe('replace', () => {
+    let array = ['a', 'b', 'c'];
+    let ms = Microstates.from(MS.Array, array);
+    it('replaces first element', () => {
+      expect(ms.transitions.replace('a', 'd')).toEqual(['d', 'b', 'c']);
+    });
+    it('does not throw when replacing non-existing item', () => {
+      expect(() => {
+        ms.transitions.replace('e', 'd');
+      }).not.toThrow();
+    });
+    it('returns same array when value not found', () => {
+      expect(ms.transitions.replace('e', 'd')).toBe(array);
+    });
+  });
+});
