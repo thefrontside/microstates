e0cf92cdf71d313be544ccdbb4d268d98d034d5d
Taras Mankovski <tarasm@gmail.com>
2017-11-17 12:37:01 -0500

    Removed validate file

--- src/utils/validate.js
+++
@@ -1,7 +0,0 @@
-export default function validate(Type, name) {
-  if (typeof Type !== 'function') {
-    throw new Error(
-      `${name}() expects first argument to be a class, instead received ${typeof Type}`
-    );
-  }
-}
