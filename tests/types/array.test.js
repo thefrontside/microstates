6f81136d69b1a0f3b9a3f6308fc4a0c1aaeaf93a
Taras Mankovski <tarasm@gmail.com>
2017-11-19 11:53:41 -0500

    Added array test

--- tests/types/array.test.js
+++ tests/types/array.test.js
@@ -4,9 +4,19 @@ import ArrayType from '../../src/types/array';
 import Microstates, * as MS from '../../src';
 
 describe('array', () => {
+  let array = ['a', 'b', 'c'];
+  let ms = Microstates(MS.Array, array);
+  describe('filter', () => {
+    it('removes items', () => {
+      expect(ms.filter(v => v !== 'a').valueOf()).toEqual(['b', 'c']);
+    });
+  });
+  describe('map', () => {
+    it('applies to every item', () => {
+      expect(ms.map(v => v.toUpperCase()).valueOf()).toEqual(['A', 'B', 'C']);
+    });
+  });
   describe('replace', () => {
-    let array = ['a', 'b', 'c'];
-    let ms = Microstates(MS.Array, array);
     it('replaces first element', () => {
       expect(ms.replace('a', 'd').valueOf()).toEqual(['d', 'b', 'c']);
     });
