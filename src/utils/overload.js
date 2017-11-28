30811378317a6baec80ccab74c73b402a5047893
Taras Mankovski <tarasm@gmail.com>
2017-11-28 11:07:55 -0500

    Removed Class Properties syntax from overload

--- src/utils/overload.js
+++ src/utils/overload.js
@@ -1,5 +1,9 @@
 export default function overload(Type, property, value) {
   return class extends Type {
-    [property] = value;
+    constructor(...args) {
+      let tmp = super(...args);
+      this[property] = value;
+      return tmp;
+    }
   };
 }
