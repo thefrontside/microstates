bb323326df8794f8d6e1833240ec1a82c5274da3
Taras Mankovski <tarasm@gmail.com>
2017-11-28 10:45:38 -0500

    Added description of microstates object

--- src/microstates.js
+++ src/microstates.js
@@ -2,6 +2,27 @@ import { map, append } from 'funcadelic';
 
 import Microstate from './utils/microstate';
 
+/**
+ * Invoking Microstates will return an object with the following shape.
+ *
+ * {
+ *  Type,                     // structure of current microstate
+ *  value,                    // initial value
+ *  valueOf() {               // initial value
+ *    return value;
+ *  }
+ *
+ *  // General use API
+ *  states,                   // instantiated states
+ *  ...transitions.collapsed  // spread collapsed transitions for chaining
+ *
+ *  // Customization API
+ *  transitions,              // uncollapsed transitions tree
+ * }
+ *
+ * @param {*} Type
+ * @param {*} value
+ */
 export default function Microstates(Type, value) {
   let microstate = new Microstate(Type, value);
   let properties = append(
