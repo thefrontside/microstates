4a1c5daac59024bf31c25ff19fea4278f2c256d4
Taras Mankovski <tarasm@gmail.com>
2017-12-14 17:24:42 -0500

    Added Microstate Functor for mapping transitions

--- tests/microstate.test.js
+++ tests/microstate.test.js
@@ -1,5 +1,5 @@
 import 'jest';
-
+import { map } from 'funcadelic';
 import microstate, * as MS from '../src';
 
 describe('microstate', () => {
@@ -789,6 +789,23 @@ describe('microstate', () => {
           },
         });
       });
+    });
+  });
+  describe('microstate transition mapping', function() {
+    class MyType {
+      myProp = MS.String;
+    }
+    let actions = map(transition => (...args) => transition(...args), microstate(MyType));
+    it('keeps objects as objects', () => {
+      expect(actions).toMatchObject({
+        set: expect.any(Function),
+        myProp: {
+          set: expect.any(Function),
+        },
+      });
+    });
+    it('has working transitions', () => {
+      expect(actions.myProp.set('foo').state).toEqual({ myProp: 'foo' });
     });
   });
 });
