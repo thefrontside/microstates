ad05f7ebd2f0822235a267a4ca299f73711f168f
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-12-18 10:08:06 +0530

    Setup prettier on lint-staged.
    - Replace yarn with package-lock since yarn caches precommit state
    - entries in `.gitignore` are simplified
    - Run prettier on all the files

--- bin/monkey-patch-funcadelic.js
+++ bin/monkey-patch-funcadelic.js
@@ -5,4 +5,4 @@ const funcadelicPackageFilename = 'node_modules/funcadelic/package.json';
 let pkg = json.readFileSync(funcadelicPackageFilename);
 json.writeFileSync(`${funcadelicPackageFilename}.original`, pkg, { spaces: 2 });
 pkg.main = pkg.module;
-json.writeFileSync(funcadelicPackageFilename, pkg, {spaces: 2});
+json.writeFileSync(funcadelicPackageFilename, pkg, { spaces: 2 });
--- bin/un-monkey-patch-funcadelic.js
+++ bin/un-monkey-patch-funcadelic.js
@@ -4,4 +4,4 @@ const funcadelicPackageFilename = 'node_modules/funcadelic/package.json';
 const originalFuncadelicPackageFilename = 'node_modules/funcadelic/package.json.original';
 
 let pkg = json.readFileSync(originalFuncadelicPackageFilename);
-json.writeFileSync(funcadelicPackageFilename, pkg, {spaces: 2});
+json.writeFileSync(funcadelicPackageFilename, pkg, { spaces: 2 });
