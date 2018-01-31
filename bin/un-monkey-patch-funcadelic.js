3ef509f3e804dd3d312fd0bd51e386b356f11441
Taras Mankovski <tarasm@gmail.com>
2018-01-31 11:22:04 -0500

    Removed @std/esm

--- bin/un-monkey-patch-funcadelic.js
+++
@@ -1,7 +0,0 @@
-const json = require('jsonfile');
-
-const funcadelicPackageFilename = 'node_modules/funcadelic/package.json';
-const originalFuncadelicPackageFilename = 'node_modules/funcadelic/package.json.original';
-
-let pkg = json.readFileSync(originalFuncadelicPackageFilename);
-json.writeFileSync(funcadelicPackageFilename, pkg, { spaces: 2 });
