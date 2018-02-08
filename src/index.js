dfc054113f31bb18fa69538a2ffe4ecd9c9d38b6
Taras Mankovski <tarasm@gmail.com>
2018-02-08 17:20:05 -0500

    Exporting create

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './typeclasses';
-export { default } from './microstate';
+import Microstate from './microstate';
+
+export default Microstate;
+export const create = Microstate.create;
 
 export { default as Tree } from './utils/tree';
 export { default as analyze } from './structure';
--- tests/microstate.test.js
+++ tests/microstate.test.js
@@ -1,36 +1,38 @@
 import 'jest';
 import { map } from 'funcadelic';
-import Microstate from '../src';
+import Microstate, { create } from '../src';
 import { reveal } from '../src/utils/secret';
 
-describe('microstate', () => {
-  it('throws an error when a transition called state is defined', () => {
-    expect(function() {
-      Microstate.create(
-        class MyClass {
-          state() {}
-        }
-      );
-    }).toThrowError(
-      `You can not use 'state' as transition name because it'll conflict with state property on the microstate.`
+it('exports create', function() {
+  expect(create).toBeInstanceOf(Function);
+});
+
+it('throws an error when a transition called state is defined', () => {
+  expect(function() {
+    Microstate.create(
+      class MyClass {
+        state() {}
+      }
     );
+  }).toThrowError(
+    `You can not use 'state' as transition name because it'll conflict with state property on the microstate.`
+  );
+});
+it('throws an error when state property is set', () => {
+  expect(function() {
+    Microstate.create(Number).state = 10;
+  }).toThrowError(`Setting state property will not do anything useful. Please don't do this.`);
+});
+
+describe('valueOf', () => {
+  let ms;
+  beforeEach(() => {
+    ms = Microstate.create(Number, 10);
   });
-  it('throws an error when state property is set', () => {
-    expect(function() {
-      Microstate.create(Number).state = 10;
-    }).toThrowError(`Setting state property will not do anything useful. Please don't do this.`);
+  it('returns passed in value of', () => {
+    expect(ms.valueOf()).toBe(10);
   });
-  
-  describe('valueOf', () => {
-    let ms;
-    beforeEach(() => {
-      ms = Microstate.create(Number, 10);
-    });
-    it('returns passed in value of', () => {
-      expect(ms.valueOf()).toBe(10);
-    });
-    it('is not enumerable', () => {
-      expect(Object.keys(ms).indexOf('valueOf')).toBe(-1);
-    });
+  it('is not enumerable', () => {
+    expect(Object.keys(ms).indexOf('valueOf')).toBe(-1);
   });
 });
