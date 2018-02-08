361f46e7fdd9fb368d1e3ff0a41a67f6e523ee5b
Taras Mankovski <tarasm@gmail.com>
2018-02-08 18:12:18 -0500

    Destructing create

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './typeclasses';
 import Microstate from './microstate';
 
 export default Microstate;
-export const create = Microstate.create;
+export const { create } = Microstate;
 
 export { default as Tree } from './utils/tree';
 export { default as analyze } from './structure';
