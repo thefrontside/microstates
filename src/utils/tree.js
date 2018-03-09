e9d09a85c0207e906e1973b653b255f9a4955d42
Charles Lowell <cowboyd@frontside.io>
2018-03-09 13:34:31 -0700

    Fix Array transitions
    With the advent of parameterized types, the tree can extend _below_
    the Array type, so it isn't enough just to work with the state or the
    value. We need to keep the tree in sync.
    
    This implements a `splice` transition on the `ArrayType` which
    actually modifies the microstate tree to keep in sync with the
    value, and then `push`, `pop`, and `filter`, are implemented in terms
    of `splice`. Any nodes that can be shared are shared.
    
    This is clearly not optimal, but we need to see what tree modification
    patterns look like for Object and others before abstracting.

--- src/utils/tree.js
+++ src/utils/tree.js
@@ -32,3 +32,37 @@ export default class Tree {
     }
   }
 }
+
+/**
+ * Turn any structure tree into a root tree.
+ *
+ * Every node in a tree knows its path. This path is what identifies
+ * its context in the containing tree.
+ *
+ * This lets you take any tree, sitting at any context and make it
+ * "context free". I.e. converts it into a root.
+ *
+ * @param {Tree} tree - the tree to isolate
+ * @returns {Tree} - a tree just like `tree`, but now a root.
+ */
+export function prune(tree) {
+  let prefix = tree.data.path;
+  return map(node => append(node, { path: node.path.slice(prefix.length)}), tree);
+}
+
+/**
+ * Change the path of a tree.
+ *
+ * This lets you take any tree, sitting at any context and prefix the context with
+ * additional path.
+ *
+ * @param {*} tree
+ * @param {*} path
+ */
+export function graft(path, tree) {
+  if (path.length === 0) {
+    return tree;
+  } else {
+    return map(node => append(node, { path: [...path, ...node.path]}), tree);
+  }
+}
