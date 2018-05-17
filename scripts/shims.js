d666e2ae9f142a0a6a3c86433677fa39ab3b7f2a
Taras Mankovski <tarasm@gmail.com>
2018-05-17 10:13:20 -0400

    Added Object.getOwnPropertyDescriptors polyfill (#117)

---
+++ scripts/shims.js
@@ -0,0 +1,3 @@
+import getOwnPropertyDescriptors from 'object.getownpropertydescriptors';
+
+global.Object.getOwnPropertyDescriptors = global.Object.getOwnPropertyDescriptors || getOwnPropertyDescriptors;
