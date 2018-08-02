7dcbeaff4370a93aec026e9a665637db7d95d739
Charles Lowell <cowboyd@frontside.io>
2018-08-02 11:11:26 -0600

    Workaround non-deterministic order for registering syntactic sugar
    Because the list of syntax sugar is mutable and is appended to from
    different portions of the codebase, some sugars will not be registered
    in the right order.
    
    This isn't a long-term fix for the issue, but it does resolve it in
    the short term.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/** HOTFIX
+* workaround for https://github.com/microstates/microstates.js/issues/186
+* this lines should not be necessary because syntactic sugar precedence should be
+* deterministic.
+*/
+import "./src/types";
+/** END HOTFIX */
+
 export { create } from './src/microstates';
 export { default as from } from './src/literal';
 export { map, filter, reduce } from './src/query';
