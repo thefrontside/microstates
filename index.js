1478dba005f41d0f52e04896bd068b16a81f89d3
Charles Lowell <cowboyd@frontside.io>
2018-11-13 12:02:38 -0700

    Add exports needed by microstates-redux.
    There are better ways to accomplish this, but this has to be published
    somewhere in order for it to actually work.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ export { create } from './src/microstates';
 export { default as from } from './src/literal';
 export { map, filter, reduce } from './src/query';
 export { default as Store } from './src/identity';
-export { metaOf, valueOf } from './src/meta';
+export { promap, mount, metaOf, pathOf, valueOf, Meta } from './src/meta';
+export { isArrayType } from './src/types/array';
 export * from './src/types';
