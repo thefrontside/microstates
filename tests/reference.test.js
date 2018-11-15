a89f3d7fa70bd604e40f6f3a6b65be31b432c6e4
Wil Wilsman <wil@frontside.io>
2018-11-15 16:14:17 -0600

    Start reference implementation

--- tests/reference.test.js
+++ tests/reference.test.js
@@ -1,11 +1,22 @@
 import expect from 'expect';
+import { create } from '..';
 
-describe('References', () => {
+const {
+  getOwnPropertyNames
+} = Object;
+
+function reference(Type, fn) {
+  let ref = fn(this);
+  return class extends Type {}
+}
+
+describe.only('References', () => {
   class Person {}
+
   class PersonTable {
     nextId = Number;
     records = { Person };
-    createRecord(attrs) {
+    createRecord(attrs = {}) {
       let id = this.nextId.state;
 
       return this
@@ -19,26 +30,32 @@ describe('References', () => {
   }
 
   class CarTable {
-    db = reference(Db);
+    nextId = Number;
     records = { Car };
-    createRecord() {
+    createRecord(attrs = {}) {
+      let id = this.nextId.state;
+      let personId = intermediate.db.people.nextId.state;
       let intermediate = this.db.people.createRecord();
-      intermediate.db
-      return this.db.people
+
+      return intermediate
+        .nextId.increment()
+        .records.put(id, { personId, ...attrs })
     }
   }
+
   class Db {
     people = PersonTable;
-    cars = CarTable;
+    cars = reference(CarTable, db => ({ db }));
   }
 
   let db;
   beforeEach(function() {
     db = create(Db)
       .cars.createRecord();
-
   });
 
-  it('creates a new DB with both the car and its owner')
-
+  it('creates a new DB with both the car and its owner', () => {
+    expect(getOwnPropertyNames(db.cars.records).length).toEqual(1);
+    expect(getOwnPropertyNames(db.people.records).length).toEqual(1);
+  });
 });
