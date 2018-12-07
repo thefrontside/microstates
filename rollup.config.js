a5e7341c059c8d755fb1b6a3befda3c878a9caaa
Taras Mankovski <tarasm@gmail.com>
2018-12-07 15:04:06 -0500

    Added UMD build that does not transpile classes (#294)

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ const pkg = require("./package.json");
 const babel = require("rollup-plugin-babel");
 const filesize = require("rollup-plugin-filesize");
 const resolve = require("rollup-plugin-node-resolve");
+const commonjs = require('rollup-plugin-commonjs');
+const replace = require('rollup-plugin-replace');
 
 const external = [
   "funcadelic",
@@ -25,6 +27,41 @@ const babelPlugin = babel({
 const fileSize = filesize();
 
 module.exports = [
+  {
+    input: "index.js",
+    output: {
+      name: 'Microstates',
+      file: pkg.unpkg,
+      format: "umd"
+    },
+    plugins: [
+      replace({
+        "process.env.NODE_ENV": JSON.stringify('production')
+      }),
+      resolve({
+        module: false,
+        main: true
+      }),
+      babel({
+        babelrc: false,
+        comments: false,
+        plugins: ["@babel/plugin-proposal-class-properties"],
+        presets: [
+          [
+            "@babel/preset-env",
+            {
+              targets: {
+                chrome: "58",
+              },
+              modules: false
+            }
+          ]
+        ]
+      }),
+      commonjs(),
+      fileSize
+    ]
+  },
   {
     input: "index.js",
     external,
