f332ab5e8ff6e433dcd8be21a78c32fd6b066f0a
Charles Lowell <cowboyd@frontside.io>
2019-04-26 10:01:23 -0600

    Export `relationship()` function.

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ export { default as from } from './src/literal';
 export { map, filter, reduce, find } from './src/query';
 export { default as Store } from './src/identity';
 export { metaOf, valueOf } from './src/meta';
+export { relationship } from './src/relationship.js';
 export * from './src/types';
